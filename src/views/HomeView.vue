3586cac3f0cd12d073cd5e17d1b3baa3722d7fd3
Kieffer Ganza <kiefferganza23@gmail.com>
2024-11-04 22:01:39 +0800

    add eslint rule

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,12 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Login />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Login from '@/components/Auth/Login.vue';
 
 export default {
   name: 'HomeView',
